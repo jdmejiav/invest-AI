a062a2715281055fe744a85f137918d26b296585
jdmejiav <jdmejiav@eafit.edu.co>
2021-09-24 00:54:01 -0500

    predict stock price machine learning

---
+++ predict_stock_machinelearning.py
@@ -0,0 +1,11 @@
+import math
+import pandas_datareader as web
+import pandas as d
+import numpy as p
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import matplotlib.pyplot as pit
+
+
+pit.style.use ('fivethirtyeight')
